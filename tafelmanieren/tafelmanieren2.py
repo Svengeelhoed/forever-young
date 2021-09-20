208b4ecfb18ff466e23a7ce9ee55fff423c318e1
Svengeelhoed <99069588@mydavinci.nl>
2021-09-20 11:28:12 +0200

    tafelmanieren gemaakt

---
+++ tafelmanieren/tafelmanieren2.py
@@ -0,0 +1,4 @@
+for i in range(30,0,-1):
+    print(i - 1)
+    
+print("oh shit de raket is gecrashed")
---
+++ tafelmanieren/tafelmanieren3.py
@@ -0,0 +1,4 @@
+for i in range(0,12):
+    print(str(i * 1) + (" AM"))
+for o in range(12,25):
+    print(str(o * 1) + (" PM"))
